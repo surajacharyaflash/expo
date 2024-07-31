89f3d37e0498e31d758bedb3bf4a51582c87be02
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2024-07-31 11:33:18 +0200

    [ios][modules] Experimental support for embedding SwiftUI views

--- packages/expo-linear-gradient/ios/LinearGradientModule.swift
+++ packages/expo-linear-gradient/ios/LinearGradientModule.swift
@@ -2,27 +2,61 @@
 
 import CoreGraphics
 import ExpoModulesCore
+import SwiftUI
 
 public class LinearGradientModule: Module {
   public func definition() -> ModuleDefinition {
     Name("ExpoLinearGradient")
 
-    View(LinearGradientView.self) {
-      Prop("colors") { (view: LinearGradientView, colors: [CGColor]) in
-        view.gradientLayer.setColors(colors)
-      }
+    View(LinearGradientSwiftUIView.self)
 
-      Prop("startPoint") { (view: LinearGradientView, startPoint: CGPoint?) in
-        view.gradientLayer.setStartPoint(startPoint)
-      }
+//    View(LinearGradientView.self) {
+//      Prop("colors") { (view: LinearGradientView, colors: [CGColor]) in
+//        view.gradientLayer.setColors(colors)
+//      }
+//
+//      Prop("startPoint") { (view: LinearGradientView, startPoint: CGPoint?) in
+//        view.gradientLayer.setStartPoint(startPoint)
+//      }
+//
+//      Prop("endPoint") { (view: LinearGradientView, endPoint: CGPoint?) in
+//        view.gradientLayer.setEndPoint(endPoint)
+//      }
+//
+//      Prop("locations") { (view: LinearGradientView, locations: [CGFloat]?) in
+//        view.gradientLayer.setLocations(locations)
+//      }
+//    }
+  }
+}
 
-      Prop("endPoint") { (view: LinearGradientView, endPoint: CGPoint?) in
-        view.gradientLayer.setEndPoint(endPoint)
-      }
+public final class LinearGradientProps: ExpoSwiftUI.ViewProps {
+  @Field
+  var colors: [Color] = []
 
-      Prop("locations") { (view: LinearGradientView, locations: [CGFloat]?) in
-        view.gradientLayer.setLocations(locations)
-      }
+  @Field
+  var startPoint: UnitPoint = UnitPoint(x: 0.5, y: 0.0)
+
+  @Field
+  var endPoint: UnitPoint = UnitPoint(x: 0.5, y: 1.0)
+
+  @Field
+  var locations: [Double] = []
+}
+
+struct LinearGradientSwiftUIView: ExpoSwiftUI.View {
+  @EnvironmentObject var props: LinearGradientProps
+
+  var body: some View {
+    ZStack {
+      LinearGradient(
+        colors: props.colors,
+        startPoint: props.startPoint,
+        endPoint: props.endPoint
+      )
+      Text("SwiftUI in Expo 🔥")
+        .font(.system(size: 30))
+        .foregroundColor(.white)
     }
   }
 }
